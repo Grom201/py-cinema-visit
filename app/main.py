4a07145ae9043378c3d2cf56b5bfcc7aa43cfa73
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-04-15 17:15:21 +0200

    part of issue done, to save progress

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+#from app.cinema.bar import CinemaBar
+from cinema import bar
+
 # write your imports here
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    for customer in customers:
+        CinemaBar.sell_product(customer.name, customer.food)
